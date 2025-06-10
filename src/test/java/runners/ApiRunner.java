95123a3551708b050ce3dfbed7d0a0ec18e71960
SyntaxTechnologies <moazzam@syntaxtechs.com>
2025-06-09 20:50:25 -0400

    final

---
+++ src/test/java/runners/ApiRunner.java
@@ -0,0 +1,21 @@
+package runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        //path to all the feature file
+        //   features = "src/test/resources/features/Login.feature"
+        // features = "src/test/resources/features/EmployeeSearch.feature"
+        features = "src/test/resources/features/",
+        //name of  the package where all the step definitions available
+        glue = "APISteps",
+        //if we set the value to true, it stops execution and scan all the step definitions and gives missing steps
+        dryRun = false,
+        tags = "@api",
+        //html report
+        plugin = {"pretty","html:target/cucumber.html"}
+)public class ApiRunner {
+}
